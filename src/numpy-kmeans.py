1eceba9e16c7885fd994a123ec1b71363fac81ff
Christopher <chris@thecarmans.com>
2019-02-21 17:00:58 +0100

    Made into classes

--- src/numpy-kmeans.py
+++ src/numpy-kmeans.py
@@ -1,38 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def init_random_centroids(points, k):
-    centroids_copy = points.copy()
-    np.random.shuffle(centroids_copy)
-    return centroids_copy[:k]
+class bohr_kmeans:
+
+    def __init__(self):
+        pass
+
+    def init_random_centroids(self, points, k):
+        centroids_copy = points.copy()
+        np.random.shuffle(centroids_copy)
+        return centroids_copy[:k]
 
 
-def closest_centroids(points, centroids):
-    #distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
-    distances = np.linalg.norm(points-centroids[:,np.newaxis], axis = 2 )
-    return np.argmin(distances, axis = 0)
+    def closest_centroids(self, points, centroids):
+        #distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
+        distances = np.linalg.norm(points-centroids[:,np.newaxis], axis = 2 )
+        return np.argmin(distances, axis = 0)
 
 
-def move_centroids(points, closest, centroids):
-    return np.array([points[closest==k].mean(axis=0) for k in range(centroids.shape[0])])
+    def move_centroids(self, points, closest, centroids):
+        return np.array([points[closest==k].mean(axis=0) for k in range(centroids.shape[0])])
 
 
-def numpy_kmeans(points, k, epsilon=0.00001):
+    def numpy_kmeans(self, points, k, epsilon=0.00001):
 
-    centroids = init_random_centroids(points, k)
-    centroids_old = np.zeros(centroids.shape)
+        centroids = self.init_random_centroids(points, k)
+        centroids_old = np.zeros(centroids.shape)
 
-    iterations = 0
+        iterations = 0
 
-    while np.linalg.norm(centroids - centroids_old) > epsilon:
+        while np.linalg.norm(centroids - centroids_old) > epsilon:
 
-        centroids_old = centroids
+            centroids_old = centroids
 
-        closest = closest_centroids(points, centroids)
-        centroids = move_centroids(points, closest, centroids)
-        iterations += 1
+            closest = self.closest_centroids(points, centroids)
+            centroids = self.move_centroids(points, closest, centroids)
+            iterations += 1
 
-    return closest, centroids, iterations
+        return closest, centroids, iterations
 
 
 if __name__ == "__main__":
@@ -41,9 +46,12 @@ if __name__ == "__main__":
                   (np.random.randn(50, 2) * 0.5443 + np.array([-0.5, -0.5]))))
 
 
-    closest, centroids, iteratins = numpy_kmeans(points, 3)
+    my_kmeans = bohr_kmeans()
 
-    print(centroids)
+    closest, centroids, iterations = my_kmeans.numpy_kmeans(points, 3)
+
+    print(closest)
+
     plt.scatter(points[:,0],points[:,1], c=closest)
 
     plt.scatter(centroids[:,0], centroids[:,1], marker = "X", s=400, c = 'r')
