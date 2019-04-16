1c23aa0fdda2e61fb8c46c555fdd4935130af700
Christopher <chris@thecarmans.com>
2019-04-16 13:03:01 +0200

    Made cleaner

--- src/centroids_closest.c
+++ src/centroids_closest.c
@@ -7,36 +7,34 @@
 
 int get_index(int x, int y, int width) { return x + (y * width); }
 
+int argmin(double *start, int end)
+{
+  double minimum = start[0];
+  int index;
+
+  for (int i = 1; i < end; ++i)
+  {
+    if (start[i] < minimum)
+    {
+      minimum = start[i];
+      index = i;
+    }
+  }
+  return index;
+}
+
 void execute(double *dist, int64_t *res)
 {
   int n_points = 0;
   int n_k = 0;
   int i;
 
-#pragma omp parallel
-   {
-    double minimum;
-    int location = 0;
+  #pragma omp parallel for
+  for (i = 0; i < n_points; i++)
+  {
+    int row_index = get_index(0, i, n_k);
+    int location = argmin(&dist[row_index], n_k);
+    res[i] = location;
+  }
 
-   #pragma omp for
-    for (i = 0; i < n_points; i++)
-    {
-      int row_index = get_index(0, i, n_k);
-      minimum = dist[row_index];
-      location = 0;
-
-
-      for (int j = 1; j < n_k; j++)
-      {
-	if (dist[row_index+j] < minimum)
-	{
-	  minimum = dist[row_index+j];
-	  location = j;
-	}
-      }
-
-      res[i] = location;
-
-    }
-    }
 }
