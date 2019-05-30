70fd634e6f3a36d99cb0683eb6071955bf39078b
Christopher Carman <cca@nelson.esci.nbi.dk>
2019-05-30 16:17:41 +0200

    userkernel works

--- src/user-kernels/centroids_closest_opencl.c
+++ src/user-kernels/centroids_closest_opencl.c
@@ -1,5 +1,5 @@
 #pragma OPENCL EXTENSION cl_khr_fp64 : enable
-__kernel void execute(__global double *dist, __global int *res, __global double *min_dist)
+kernel void execute(global double *dist, global double *min_dist, global long *res)
 {
   int n_k = 0;
   int n_points = 0;
@@ -12,17 +12,19 @@ __kernel void execute(__global double *dist, __global int *res, __global double 
   int row_index = i * n_k;
 
   double minimum = dist[row_index];
-  int index;
+  int index = 0;
 
-  for (int j = 1; j < n_k; j++)
+  for (int j = 0; j < n_k; j++)
   {
+    
     if (dist[j + row_index] < minimum)
     {
       minimum = dist[j + row_index];
-      index = j + row_index;
+      index = j;
     }
   }
 
+  
   res[i] = index;
-  min_dist[i] = dist[res[i] + row_index];
+  min_dist[i] = dist[res[i]];
 }
