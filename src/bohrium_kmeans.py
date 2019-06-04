9dd318cd7aab36a6dac7f3c0d6658d01a6c62989
Christopher <chris@thecarmans.com>
2019-06-04 15:05:15 +0200

    updated userkernel types

--- src/bohrium_kmeans.py
+++ src/bohrium_kmeans.py
@@ -273,6 +273,7 @@ class bohrium_kmeans:
         self.kernel_move_centroids_opencl = self.kernel_move_centroids_opencl.replace("int size_labels = 0", "int size_labels = " + str(labels.shape[0]))
         self.kernel_move_centroids_opencl = self.kernel_move_centroids_opencl.replace("int dim = 0", "int dim = " + str(points.shape[1]))
         self.kernel_move_centroids_opencl = self.kernel_move_centroids_opencl.replace("int k = 0", "int k = " + str(self.k))
+        self.kernel_move_centroids_opencl = self.kernel_move_centroids_opencl.replace("dimm", "" + str(points.shape[1]))
 
         # self.mask = self.mask.replace("int rows = 0", "int rows = " + str(self.k))
         # self.mask = self.mask.replace("int cols = 0", "int cols = " + str(labels.shape[0]))
