31f86ce2b7655993b591dcd1aca457d73fa6e06a
Christopher <chris@thecarmans.com>
2019-06-04 14:54:06 +0200

    hello 3

--- src/bohrium_kmeans.py
+++ src/bohrium_kmeans.py
@@ -277,7 +277,7 @@ class bohrium_kmeans:
         # self.mask = self.mask.replace("int rows = 0", "int rows = " + str(self.k))
         # self.mask = self.mask.replace("int cols = 0", "int cols = " + str(labels.shape[0]))
         # mask2 = bh.zeros(self.k*labels.shape[0], dtype=bh.int64).reshape(self.k,labels.shape[0])
-        bh.user_kernel.execute(self.kernel_move_centroids_openc, [labels, old_labels, points, out], tag="opencl", param={"global_work_size": [self.k], "local_work_size": [1]})
+        bh.user_kernel.execute(self.kernel_move_centroids_opencl, [labels, old_labels, points, out], tag="opencl", param={"global_work_size": [self.k], "local_work_size": [1]})
 
         # out = bh.array()
         # out = bh.zeros_like(centroids)
